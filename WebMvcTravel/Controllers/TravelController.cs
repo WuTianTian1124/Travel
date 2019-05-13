b40a2de46794122977f01c5ea321a94541a69f3d
8615910461621 <1925618905@qq.com>
2019-05-13 18:59:46 +0800

    HYK

---
+++ WebMvcTravel/Controllers/TravelController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace WebMvcTravel.Controllers
+{
+    public class TravelController : Controller
+    {
+        // GET: Travel
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Result_List()
+        {
+            return View();
+        }
+    }
+}
